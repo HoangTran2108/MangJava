3bf1442099f8772881587c5ab1e7db2d1b3abc2f
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-01-09 14:09:51 +0700

    [Bài tập] Tìm phần tử lớn nhất trong mảng hai chiều

--- bt4/src/Main.java
+++ bt4/src/Main.java
@@ -12,15 +12,19 @@ public class Main {
 
         int[][] a = new int[m][n];
         int max = a[0][0];
+        int index1 =0;
+        int index2 =0;
         for (int i = 0; i<m ; i++){
             for (int j = 0; j<n ; j++) {
                 System.out.println("Nhập vào phần tử có vị trí " + "[" + i + "]" + "[" + j + "]");
                 a[i][j] = scanner.nextInt();
                 if (max<a[i][j]) {
                     max = a[i][j];
+                    index1 = i;
+                    index2 = j;
                 }
             }
         }
-        System.out.println("Giá trị lớn nhất trong mảng là: " + max);
+        System.out.println("Giá trị lớn nhất trong mảng là: " + max + " tại toạ độ " + "[" + index1 + "]" + "[" + index2 + "]");
     }
 }
